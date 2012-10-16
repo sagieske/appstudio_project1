3518095f83f9effacca36db38039d2bea9dabbdf
sagieske <sharongieske@gmail.com>
2012-10-16 10:01:40 -0400

    footer and header. Jquery inserted

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,10 +4,11 @@ Doing that here means that most pages have a link to themselves, though. -->
 <FORM>
 <!-- This might be a bad way to do it. I had to hardcode the link...
      It might be possible to call the view method from php? -->
+<!-- 
 <INPUT TYPE="BUTTON" VALUE="Go to a list"
 ONCLICK="window.location.href='http://appstudio_project1/pages/view/list'">
 <!-- favorites and recent is just the same list for now -->
-<INPUT TYPE="BUTTON" VALUE="Go to your favorites"
+<!-- <INPUT TYPE="BUTTON" VALUE="Go to your favorites"
 ONCLICK="window.location.href='http://appstudio_project1/pages/view/list'">
 <br>
 <INPUT TYPE="BUTTON" VALUE="Recently viewed"
@@ -15,7 +16,17 @@ ONCLICK="window.location.href='http://appstudio_project1/pages/view/list'">
 <INPUT TYPE="BUTTON" VALUE="Search for something"
 ONCLICK="window.location.href='http://appstudio_project1/pages/view/search'">
 </FORM>
+-->
 <!-- Seems like a lot of buttons. Do we need all these? -->
+<a href="home" data-role="button" data-icon="star">Home</a>
+<a href="list" data-role="button" data-icon="star">List</a>
+<a href="search" data-role="button" data-icon="star">Search</a>
+<a href="list2" data-role="button" data-icon="star">List</a>
+
+<br><div id=footer><br>
+    <strong>&copy; Arjen Swellengrebel en Sharon Gieske 2012</strong>
+</div>
+</div>
 
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,11 +1,21 @@
 <!doctype html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+        <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
         <title>App title</title>
     </head>
     <body>
-        <a href='http://appstudio_project1/pages/view/home'>
-        <h1>APP TITLE</h1>
-        </a>
-        
-        <hr/>
+        <div id=container data-role="page" data-theme="b">
+            <div id=header data-role="header">
+                <div id=title>
+                    <?php echo $title; ?>
+                </div>
+            </div>
+            <div data-role="content">
+                <a href='http://appstudio_project1/pages/view/home'>
+                <h1>APP TITLE</h1>
+                </a>
+                <hr/>
